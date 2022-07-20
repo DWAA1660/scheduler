dc392987c28d91a40d8c8b23faf44dc7f9244217
DWAA1660 <matnip12345@gmail.com>
2022-07-19 20:02:06 -0600

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,4 +1,6 @@
 import motor.motor_asyncio, motor
 
 global CLIENT
-CLIENT = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/scheduler")
+#CLIENT = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/scheduler")
+CLIENT = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://DWAA:Pinta123@schedulercluster.sfos0.mongodb.net/")
+characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
